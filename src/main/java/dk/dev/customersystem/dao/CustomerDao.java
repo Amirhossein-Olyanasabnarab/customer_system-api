d1ae4ae26920a723082bc9532c534c551184a4e0
amirholya01 <amirholya01@gmail.com>
2025-07-08 16:18:38 +0330

    Customer Dao

---
+++ src/main/java/dk/dev/customersystem/dao/CustomerDao.java
@@ -0,0 +1,15 @@
+package dk.dev.customersystem.dao;
+
+import dk.dev.customersystem.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+    Customer save(Customer customer);
+    void delete(Long id);
+    Optional<Customer> findById(Long id);
+    List<Customer> findAll();
+    List<Customer> findByNameIgnoreCase(String name);
+    boolean existsById(Long id);
+}
