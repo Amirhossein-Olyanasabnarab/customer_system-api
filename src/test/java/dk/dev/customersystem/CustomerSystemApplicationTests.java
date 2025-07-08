c54089b471f18d84a7357a54e8c40ac7fc0d88b7
amirholya01 <amirholya01@gmail.com>
2025-07-08 16:00:06 +0330

    first commit

---
+++ src/test/java/dk/dev/customersystem/CustomerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.dev.customersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
