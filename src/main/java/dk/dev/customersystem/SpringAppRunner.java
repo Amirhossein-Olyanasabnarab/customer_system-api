539b297bf00d8039aa7279ead6860f2de51c0d98
amirholya01 <amirholya01@gmail.com>
2025-07-10 17:22:55 +0330

    CheckProfile

--- src/main/java/dk/dev/customersystem/SpringAppRunner.java
+++ src/main/java/dk/dev/customersystem/SpringAppRunner.java
@@ -1,8 +1,14 @@
 package dk.dev.customersystem;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class SpringAppRunner {
@@ -10,8 +16,21 @@ public class SpringAppRunner {
     public static void main(String[] args) {
 
         SpringApplication app = new SpringApplication(SpringAppRunner.class);
-        app.setWebApplicationType(WebApplicationType.NONE);
+        if (checkProfile("console")) {
+            app.setWebApplicationType(WebApplicationType.NONE);
+        }
         app.run(args);
     }
 
+    private static boolean checkProfile(String profile) {
+        try {
+            ClassPathResource resource = new ClassPathResource("application.properties");
+            Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+            String activeProfile = properties.getProperty("spring.profiles.active");
+            return activeProfile != null && activeProfile.contains(profile);
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
