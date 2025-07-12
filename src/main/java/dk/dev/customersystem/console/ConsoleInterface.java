65f928296d51df0d0c394c80d81a3bc5f661a685
amirholya01 <amirholya01@gmail.com>
2025-07-12 18:33:38 +0330

    Using CustomerNotFoundException in ConsoleInterface-getCustomerById

--- src/main/java/dk/dev/customersystem/console/ConsoleInterface.java
+++ src/main/java/dk/dev/customersystem/console/ConsoleInterface.java
@@ -6,6 +6,7 @@ import dk.dev.customersystem.dto.CustomerDto;
 import dk.dev.customersystem.dto.LegalCustomerDto;
 import dk.dev.customersystem.dto.RealCustomerDto;
 import dk.dev.customersystem.enums.CustomerType;
+import dk.dev.customersystem.exception.CustomerNotFoundException;
 import dk.dev.customersystem.facade.CustomerFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -150,8 +151,12 @@ public class ConsoleInterface {
     private void viewCustomerById() {
         System.out.print("Enter customer ID: ");
         Long id = Long.parseLong(scanner.nextLine());
-        CustomerDto customer = facade.getCustomerById(id);
-        printJsonObject(customer);
+        try {
+            CustomerDto customer = facade.getCustomerById(id);
+            printJsonObject(customer);
+        }catch (CustomerNotFoundException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     private void viewAllCustomers() {
