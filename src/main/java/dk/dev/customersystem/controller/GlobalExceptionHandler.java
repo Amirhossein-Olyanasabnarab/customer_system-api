d4956d9b2d894b4e41466af2cc04012905b65741
amirholya01 <amirholya01@gmail.com>
2025-07-15 19:59:31 +0330

    Generating RestControllerAdvice for duplicated customer

--- src/main/java/dk/dev/customersystem/controller/GlobalExceptionHandler.java
+++ src/main/java/dk/dev/customersystem/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package dk.dev.customersystem.controller;
 
 import dk.dev.customersystem.dto.ErrorResponse;
 import dk.dev.customersystem.exception.CustomerNotFoundException;
+import dk.dev.customersystem.exception.DuplicatedCustomerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,4 +17,12 @@ public class GlobalExceptionHandler {
                 exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(DuplicatedCustomerException.class)
+    public ResponseEntity<ErrorResponse> handlerDuplicatedCustomerException(DuplicatedCustomerException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(),
+                exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
 }
--- src/main/java/dk/dev/customersystem/service/CustomerService.java
+++ src/main/java/dk/dev/customersystem/service/CustomerService.java
@@ -69,4 +69,5 @@ public class CustomerService {
         }else
             return customers;
     }
+
 }
