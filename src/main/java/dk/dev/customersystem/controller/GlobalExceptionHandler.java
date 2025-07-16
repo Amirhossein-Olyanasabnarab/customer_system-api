9aafbe62743692536bf0007672438b6d0d00d3a6
amirholya01 <amirholya01@gmail.com>
2025-07-16 15:58:20 +0330

    JdbcSQLIntegrityConstraintViolationException-ExceptionHandler-RestControllerAdvice for Jdbc

--- src/main/java/dk/dev/customersystem/controller/GlobalExceptionHandler.java
+++ src/main/java/dk/dev/customersystem/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package dk.dev.customersystem.controller;
 import dk.dev.customersystem.dto.ErrorResponse;
 import dk.dev.customersystem.exception.CustomerNotFoundException;
 import dk.dev.customersystem.exception.DuplicatedCustomerException;
+import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,6 +24,12 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(),
                 exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(JdbcSQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleSQLIntegrityConstraintViolationException(JdbcSQLIntegrityConstraintViolationException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                exception.getOriginalMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
