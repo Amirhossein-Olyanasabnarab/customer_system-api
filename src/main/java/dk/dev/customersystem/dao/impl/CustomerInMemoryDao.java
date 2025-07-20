a2c9654e2cf23bef4cf6dc0b9b3d14419dfe214c
amirholya01 <amirholya01@gmail.com>
2025-07-20 16:51:19 +0330

    Implementation ConstraintViolationException in inMemoryDao

--- src/main/java/dk/dev/customersystem/dao/impl/CustomerInMemoryDao.java
+++ src/main/java/dk/dev/customersystem/dao/impl/CustomerInMemoryDao.java
@@ -2,12 +2,17 @@ package dk.dev.customersystem.dao.impl;
 
 import dk.dev.customersystem.dao.CustomerDao;
 import dk.dev.customersystem.model.Customer;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,9 +22,18 @@ public class CustomerInMemoryDao implements CustomerDao {
 
     private final AtomicLong currentId = new AtomicLong(0);
     private final Map<Long, Customer> customers = new ConcurrentHashMap<>();
+    private final Validator validator;
+    @Autowired
+    public CustomerInMemoryDao(Validator validator) {
+        this.validator = validator;
+    }
 
     @Override
     public Customer save(Customer customer) {
+        Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
         if(!existsById(customer.getId())) {
             Long id = currentId.incrementAndGet();
             customer.setId(id);
