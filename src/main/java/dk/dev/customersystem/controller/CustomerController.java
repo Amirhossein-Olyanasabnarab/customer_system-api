ac07a7e2f7a6e28f429a837c470d4f01b3a65be5
amirholya01 <amirholya01@gmail.com>
2025-07-21 08:46:59 +0330

    Using Valid annoration in controller

--- src/main/java/dk/dev/customersystem/controller/CustomerController.java
+++ src/main/java/dk/dev/customersystem/controller/CustomerController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -125,7 +126,7 @@ public class CustomerController {
                     }
             )
     )
-                                   @RequestBody CustomerDto customer) {
+                                  @Valid @RequestBody CustomerDto customer) {
         return facade.addCustomer(customer);
     }
 
